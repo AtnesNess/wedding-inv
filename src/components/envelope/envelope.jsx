0c1eb7ff26294cadbf8df38e22ced4b025a7bbcb
AtnesNess <atnes@yandex-team.ru>
2019-04-11 00:57:28 +0500

    New design

--- src/components/envelope/envelope.jsx
+++ src/components/envelope/envelope.jsx
@@ -4,14 +4,19 @@ import qs from 'query-string';
 
 import Letter from '../letter/letter';
 
-import sun from './sunny.svg'
-import cloud from './cloud.svg'
+import cover from './cover.jpg';
+import leaf1 from './leaf1.png';
+import leaf2 from './leaf2.png';
+import leaf3 from './leaf3.png';
+import leaf4 from './leaf4.png';
+import leaf5 from './leaf5.png';
+import leaf6 from './leaf6.png';
+import leaf7 from './leaf7.png';
+import leaves1 from './leaves1.png';
+import leaves2 from './leaves2.png';
+
 
 import './envelope.scss';
-
-const COVER_COLOR = '#fdcb6e';
-const HEAD_COLOR = '#ecba5d';
-const BACKCOVER_COLOR = '#ff7675';
 
 export default class Envelope extends React.Component {
     static propTypes = {
@@ -34,89 +39,83 @@ export default class Envelope extends React.Component {
         return (
             <section className={b('envelope', {opened})}>
                 <div className="envelope__background">
-                    <img src={sun} alt="sun" className="envelope__sun" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
-                    <img src={cloud} alt="cloud" className="envelope__cloud" />
+                    <div className="envelope__leaves-back-group"> 
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf5} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf5} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                    </div>
                 </div>
                 <div className="envelope__sheet" onAnimationEnd={this.handleAnimationEnd}>
-                    <svg
-                        className="envelope__back-cover"
-                        width="600"
-                        height="400"
-                        viewBox="0 0 160 105"
-                    >
-                        <g transform="translate(0, 0)">
-                            <rect
-                                width="158.75"
-                                height="55"
-                                x="0"
-                                y="0.5"
-                                fill={COVER_COLOR}
-                            />
-                            <rect
-                                width="158.75"
-                                height="105.83334"
-                                x="0"
-                                y="0.5"
-                                fill={COVER_COLOR}
-                                ry="10" />
-                            <rect
-                                width="140"
-                                height="55"
-                                x="10"
-                                y="0.5"
-                                fill={BACKCOVER_COLOR}
-                            />
-                        </g>
+                    <svg className="envelope__back-cover" width="690" height="391" viewBox="0 0 690 391" fill="none">
+                        <path d="M0 0H690V386C690 388.761 687.761 391 685 391H5C2.23858 391 0 388.761 0 386V0Z" fill="url(#cover-img)" />
+                        <path d="M344.496 116L1 1H691L344.496 116Z" stroke="#F3F3F3" fill="#F8F8F8" />
+                        <defs>
+                            <pattern id="cover-img" patternUnits="userSpaceOnUse" width="690" height="391">
+                                <image href={cover} x="0" y="0" width="690" height="391" />
+                            </pattern>
+                        </defs>
                     </svg>
-                    <Letter mix="envelope__letter" />
-                    <svg
-                        className="envelope__front-cover"
-                        width="600"
-                        height="400"
-                        viewBox="0 0 160 105"
-                    >
-                        <mask id="head-mask">
-                            <rect
-                                width="158.75"
-                                height="105.83334"
-                                x="0"
-                                y="0.5"
-                                fill="#ffffff"
-                                ry="10" />
-                            <path fill="#000000" d="M 0.27263699,0.3362463 79.37905,55.121076 159.02264,0.3362463 Z" />
-                        </mask>
-                        <g transform="translate(0, 0)">
-                            <rect
-                                mask="url(#head-mask)"
-                                width="140"
-                                height="105"
-                                x="10"
-                                y="0.5"
-                                fill={COVER_COLOR}
-                            />
-                        </g>
+                    <div className="envelope__letter-container">
+                        <Letter mix="envelope__letter" />
+                    </div>
+                    <svg className="envelope__front-cover" width="690" height="391" viewBox="0 0 690 391" fill="none">
+                        <defs>
+                            <mask id="head-mask">
+                                <path d="M0 0H690V386C690 388.761 687.761 391 685 391H5C2.23858 391 0 388.761 0 386V0Z" fill="#ffffff" />
+                                <path d="M344.496 116L1 1H691L344.496 116Z" fill="#000000" />
+                            </mask>
+                        </defs>
+                        <path
+                            d="M0 0H690V386C690 388.761 687.761 391 685 391H5C2.23858 391 0 388.761 0 386V0Z"
+                            mask="url(#head-mask)" 
+                            fill="url(#cover-img)" 
+                        />
                     </svg>
-                    <svg
-                        className="envelope__head"
-                        width="600"
-                        height="400"
-                        viewBox="0 0 160 105"
-                    >
-                        <g transform="translate(0, 0)">
-                            <path fill={HEAD_COLOR} d="M 0.27263699,0.3362463 79.37905,55.121076 159.02264,0.3362463 Z" />
-                        </g>
+                    <svg className="envelope__head"  width="690" height="391" viewBox="0 0 690 391" fill="none">
+                        <path d="M344.496 116L1 1H691L344.496 116Z" stroke="#F3F3F3" fill="url(#cover-img)" />
                     </svg>
-                     <section className="envelope__signature">
-                         <div>Отправитель:  Несмияновы Артем и Елена</div>
-                         <div>Получатель: {user || 'Любопытный человек'}</div>
+                    <section className="envelope__signature">
+                         <label className="envelope__signature-label">Отправитель:</label>
+                         <div>Несмияновы Артем и Елена</div>
+                         <label className="envelope__signature-label">Получатель:</label>
+                         <div>{user || 'Любопытный человек'}</div>
                     </section>
                 </div>    
-                <div className="envelope__shadow" />
+                <div className="envelope__foreground">
+                    <div className="envelope__right-bottom-leaves">
+                        <img className={b('envelope', 'leaf')} src={leaf1} alt="leaf" />
+                        <img className={b('envelope', 'leaf')} src={leaf2} alt="leaf" />
+                        <img className={b('envelope', 'leaf')} src={leaf3} alt="leaf" />
+                    </div>
+                    <div className="envelope__left-bottom-leaf">
+                        <img className={b('envelope', 'leaf')} src={leaves2} alt="leaves" />
+                    </div>
+                    <div className="envelope__left-top-leaf">
+                        <img className={b('envelope', 'leaf')} src={leaves1} alt="leaves" />
+                    </div>
+                    <div className="envelope__leaves-front-group"> 
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf5} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf7} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf6} alt="leaf" />
+                        <img className={b('envelope', 'leaf', {floating: true})} src={leaf4} alt="leaf" />
+                    </div>
+                </div>
             </section>
         );
     }
--- src/components/letter/letter.jsx
+++ src/components/letter/letter.jsx
@@ -6,8 +6,6 @@ import qs from "query-string";
 
 import Spin from '../spin/spin';
 
-import palette from './palette.png';
-
 import './letter.scss';
 
 export default class Letter extends React.PureComponent {
@@ -30,9 +28,9 @@ export default class Letter extends React.PureComponent {
 
         fetch('/api/guest/', {
             method: 'POST',
-          headers: {
+            headers: {
               "Content-Type": "application/json",
-          },
+            },
             body: JSON.stringify({user, count, accept: true})
         }).then((response) => {
             this.setState({
@@ -43,7 +41,7 @@ export default class Letter extends React.PureComponent {
     };
 
      handleReject = () => {
-     const {location: {search}} = window;
+        const {location: {search}} = window;
         const {count, user} = qs.parse(search);
 
         this.setState({
@@ -68,24 +66,17 @@ export default class Letter extends React.PureComponent {
     render() {
         const {mix} = this.props;
         const {loading, status} = this.state;
-        const {location: {search}} = window;
-        const {count} = qs.parse(search);
 
         if (status === null) {
              return (
                 <div className={cx('letter', mix)}>
-                     <h1 className="letter__title">Приглашение на свадьбу</h1>
-                    <p className="letter__text">
-                        <p>
-                            Хотим поделится радостью — 08.06.19 состоится наша свадьба!
-                        </p>
-                        <p>
-                            Приглашаем Вас разделить с нами счастливое событие.
-                        </p>
-                        <p>
-                            Пожалуйста, подтвердите свое присутствие до 08.05.19
-                        </p>
-                    </p>
+                    <section className={b('letter', 'text', {main: true, center: true})}>
+                        8 июня 2019 — день, когда мы станем семьей. 
+                        Приглашаем вас стать частью этого особого события
+                    </section>
+                    <section className={b('letter', 'text', {center: true})}>
+                        Пожалуйста, подтвердите свое <br /> присутствие до 08.05.19
+                    </section>
                     {loading
                         ? (
                             <div className="letter__buttons">
@@ -94,16 +85,16 @@ export default class Letter extends React.PureComponent {
                         ) : (
                             <section className="letter__buttons">
                                 <button
-                                    onClick={this.handleReject}
-                                    className={b('letter', 'button', {type: 'reject'})}
-                                >
-                                    Отклонить
-                                </button>
-                                <button
                                     onClick={this.handleAccept}
                                     className={b('letter', 'button', {type: 'approve'})}
                                 >
                                     Принять
+                                </button>
+                                <button
+                                    onClick={this.handleReject}
+                                    className={b('letter', 'button', {type: 'reject'})}
+                                >
+                                    Отклонить
                                 </button>
                             </section>
                         )}
@@ -113,49 +104,58 @@ export default class Letter extends React.PureComponent {
             return (
                 <div className={cx('letter', mix)}>
                     <h1 className="letter__title">Отлично!</h1>
-                    <p className="letter__text">
-                        <p>
-                            Теперь мы знаем, что Вы придете.
-                            Ждем 08 июня в 16:00 в банкетном зале "Пятница" по адресу
-                            г. Екатеринбург, ул. Сибирский тракт, 12
-                            (Бизнес-комплекс "Квартал"), строение 1А.
-                            <br />
-                            Убедительная просьба прийти соответствуя цветовой палитре:
-                            <img  className="letter__palette" src={palette} />
-                        </p>
-                    </p>
+                    <section className="letter__text">
+                        Теперь мы знаем, что вы придете.
+                        <div className="letter__devider" />
+                        Ждем 8 июня в 16:00 в банкетном зале «Пятница», по адресу 
+                        г. Екатеринбург, ул. Сибирский тракт, 12 (Бизнес-комплекс «Квартал»), строение 1А.
+                        <div className="letter__devider" />
+                        <a 
+                            className="letter__link" 
+                            href="https://yandex.ru/maps/54/yekaterinburg/?ll=60.651445%2C56.822449&mode=search&oid=90769320962&ol=biz&z=18"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Открыть в Яндекс.Картах 
+                        </a>
+                    </section>
+                    <div className="letter__devider" />
+                    <section className="letter__text">
+                        Нам будет приятно, если ваш наряд будет соответствовать данной цветовой палитре:
+                        <div className="letter__devider" />
+                        <div className="letter__palette">
+                            <div className="letter__palette-color" />
+                            <div className="letter__palette-color" />
+                            <div className="letter__palette-color" />
+                            <div className="letter__palette-color" />
+                            <div className="letter__palette-color" />
+                        </div>
+                    </section>
                 </div>
             );
         } else if (status === 'rejected') {
             return (
                 <div className={cx('letter', mix)}>
                     <h1 className="letter__title">Очень жаль =(</h1>
-                    <p className="letter__text">
-                        <p>
-                            Мы бы очень хотели, чтобы Вы пришли...
-                        </p>
-                        <p>
-                            И мы все еще надеемся, что Вы сможете
-                            пристутствовать на нашей свадьбе.
-                        </p>
-                        <p>
-                            Если передумаете - дайте нам знать!
-                        </p>
-                    </p>
+                    <section className={b('letter', 'text', {center: true, only: true})}>
+                        Мы бы очень хотели, чтобы Вы пришли...
+                        <div className="letter__devider" />
+                        И мы все еще надеемся, что Вы сможете
+                        пристутствовать на нашей свадьбе.
+                        <div className="letter__devider" />
+                        Если передумаете - дайте нам знать!
+                    </section>
                 </div>
             );
         } else if (status === 'error') {
             return (
                 <div className={cx('letter', mix)}>
                     <h1 className="letter__title">Что то пошло не так...</h1>
-                    <p className="letter__text">
-                        <p>
-                            Сайт сломался и не записал данные о Вашем присутствии
-                        </p>
-                        <p>
-                            Пожалуйста позвоните нам, чтобы мы отметили вас в списке гостей, если Вы собираетесь прийти
-                        </p>
-                    </p>
+                    <section className={b('letter', 'text', {center: true, only: true})}>
+                        Сайт сломался и не записал данные о Вашем присутствии
+                        <div className="letter__devider" />
+                        Пожалуйста позвоните нам, чтобы мы отметили вас в списке гостей, если Вы собираетесь прийти
+                    </section>
                 </div>
             );
         }
